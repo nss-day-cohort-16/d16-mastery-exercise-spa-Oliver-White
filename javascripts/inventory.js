9fea2e867b29d706cae53b68942a24ccb554af01
Oliver White <white_oliver@comcast.net>
2016-11-07 13:34:35 -0600

    added promise

--- javascripts/inventory.js
+++ javascripts/inventory.js
@@ -1,22 +1,24 @@
-function get_inventory() {
+var old_carlot = (function() {
 	return new Promise( (resolve, reject) => {
 		$.ajax({
-		url: "json/inventory.json"
-		}).done( function (data) {
-		resolve(data);
-	}).fail( (error) => {
-		reject(error);
-	});
+		  url: "json/inventory.json"
+		  }).done( function (data) {
+		    resolve(data);
+	    }).fail( (error) => {
+		    reject(error);
+	    });
   });
-}	
+})(old_carlot | {});
+
+console.log("promise", old_carlot );
 
 // function log_data(data) {
 // 	console.log("data from XHR", data);
 // }
 
-get_inventory()
-.then( (inventory_data) => {
-	console.log(inventory_data);
-	}, (error) => {
-	console.log("Something went wrong.", error);
-});
+// get_inventory()
+// .then( (inventory_data) => {
+// 	console.log(inventory_data);
+// 	}, (error) => {
+// 	console.log("Something went wrong.", error);
+// });
