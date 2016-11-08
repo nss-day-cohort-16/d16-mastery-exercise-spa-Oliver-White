36527f2bfa9007f47cd6c9bfa9be8f7196de7e68
Oliver White <white_oliver@comcast.net>
2016-11-07 18:53:02 -0600

    minimum viable product

--- javascripts/inventory.js
+++ javascripts/inventory.js
@@ -1,24 +1,56 @@
-var old_carlot = (function() {
-	return new Promise( (resolve, reject) => {
-		$.ajax({
-		  url: "json/inventory.json"
-		  }).done( function (data) {
-		    resolve(data);
-	    }).fail( (error) => {
-		    reject(error);
-	    });
+$.ajax({ url: "json/inventory.json" })
+    .done((data) => {
+  console.log("data", data.cars);
+  var output = document.getElementById('container');
+
+  for(var i =0, inventory=""; i < data.cars.length; i++){
+
+    if(i % 3 === 0){
+        inventory += `<div class="row">`; 
+      }
+
+      inventory += `<div class="col-md-4 card">`;
+      inventory +=  `<h3>${data.cars[i].make}</h3>` ;
+      inventory +=  `<h4>${data.cars[i].model}</h4>`;
+      inventory +=  `<p>${data.cars[i].price}</p>
+              <p class="description">${data.cars[i].description}</p></div>`;
+
+    if((i+1) % 3 ===0){
+        inventory += `</div>`;
+      }
+
+    }
+      output.innerHTML += inventory;
+      var $targetCard;
+      var $inputField;
+      $(".card").click( (event) => {
+        $inputField = $(".form-control");
+        $targetCard =  $(event.currentTarget);
+        $(".card").removeClass("clicked");
+        $targetCard.addClass("clicked");
+        $inputField.val("");
+        $inputField[0].focus();
+        $inputField.keyup( (event) => {
+          $targetCard =  $(".clicked");
+          $targetCard.find(".description").text(event.currentTarget.value);
+          console.log(event);
+        })
+      });
+      
+
+            // Do DOM manipulation stuff here
+  }).fail ( (error) => {
+      console.log(`Error getting inventory: ${error}`);
   });
-})(old_carlot || {});
 
-// console.log("promise", old_carlot );
 
-// function log_data(data) {
-// 	console.log("data from XHR", data);
-// }
 
-// get_inventory = old_carlot()
-// .then( (inventory_data) => {
-// 	console.log(inventory_data);
-// 	}, (error) => {
-// 	console.log("Something went wrong.", error);
-// });
+
+
+
+
+
+
+
+
+
--- javascripts/quiz.js
+++ javascripts/quiz.js
@@ -1,9 +1,34 @@
-get_inventory = old_carlot()
-.then( (inventory_data) => {
-	console.log(inventory_data);
-	}, (error) => {
-	console.log("Something went wrong.", error);
-});
+// console.log("data", old_carlot);
+
+// var old_carlot = (function() {
+// 	return new Promise( (resolve, reject) => {
+// 		$.ajax({
+// 		  url: "json/inventory.json"
+// 		  }).done( function (data) {
+// 		    resolve(data);
+// 	    }).fail( (error) => {
+// 		    reject(error);
+// 	    });
+//   });
+// })(old_carlot || {});
+
+
+// var carlot = $.get("json/inventory.json", function (data) {console.log(data);});
+
+
+// var carlot = (function() { 
+//   var inventory = "";
+//   inventory = $.get("json/inventory.json", function (data) {console.log(data);});
+//   })(carlot || {});
 
 
 
+
+// $(document).ready(() => {
+//   $.ajax({ url: "json/inventory.json" })
+//     .done((data) => {
+//       // Do DOM manipulation stuff here
+//   }.fail( (error) => {
+//     console.log(`Error getting inventory:' ${error}`);
+//   }
+// });
