c734f68761703a97892a45cd24aea152e0d91ace
Oliver White <white_oliver@comcast.net>
2016-11-10 10:11:34 -0600

    touched js, html, and especially css

--- javascripts/quiz.js
+++ javascripts/quiz.js
@@ -2,9 +2,9 @@
 var Carlot = (function(oldCarlot){
 
     oldCarlot.activateEvents = function(stuff) {
-      var carList = document.getElementsByClassName("styled");
-      for (var j = 0; j < carList.length; j++){
-        carList[j].addEventListener('click', addBorder);
+      var cars = document.getElementsByClassName("styled");
+      for (var j = 0; j < cars.length; j++){
+        cars[j].addEventListener('click', addBorder);
       }
       function addBorder() {
         if (event.currentTarget.className === "col-sm-4 styled"){
@@ -16,9 +16,9 @@ var Carlot = (function(oldCarlot){
         addText();
       }
       function addText () {
-        var carDivElements = document.getElementsByClassName("description");
-        for (var z = 0; z < carDivElements.length; z++){
-          carDivElements[z].addEventListener("click", changeText)
+        var carStuff = document.getElementsByClassName("description");
+        for (var a = 0; a < carDivElements.length; a++){
+          carStuff[a].addEventListener("click", changeText)
         }
         changeText();
       }
@@ -31,7 +31,7 @@ var Carlot = (function(oldCarlot){
         }
 
         function changeDescription (spot) {
-        document.getElementById('input').addEventListener('keydown', write);
+        document.getElementById('input').addEventListener('keyup', write);
         function write() {
           spot.innerHTML = document.getElementById("input").value;
         }
--- javascripts/style.js
+++ javascripts/style.js
@@ -1,11 +1,11 @@
 "use strict";
 var Carlot = (function(oldCarlot){
-  oldCarlot.resetBorder = function(item){
-    item.classList = "col-sm-4 styled";
+  oldCarlot.resetBorder = function(reset){
+    reset.classList = "col-sm-4 styled";
   },
 
-  oldCarlot.changeBorder = function(target) {
-    target.classList += " thickBorder"
+  oldCarlot.changeBorder = function(modify) {
+    modify.classList += " thickBorder"
   }
   return oldCarlot
 })(Carlot || {});
