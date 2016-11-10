a3b3b832f15932add9c6782200cc2cf1aa46d8e2
Oliver White <white_oliver@comcast.net>
2016-11-10 10:53:03 -0600

    touched css

--- javascripts/inventory.js
+++ javascripts/inventory.js
@@ -5,7 +5,7 @@ var Carlot = (function(oldCarlot){
     var createDisplay = "<div class='row'>";
     for (var i = 0; i < oldCarlot.length; i++){
       var newCar = document.createElement("div");
-      newCar.classList = "col-sm-4 styled";
+      newCar.classList = "col-sm-4 pretty";
       createDisplay = "<p class='make'>" + oldCarlot[i].year +  " " + oldCarlot[i].make + " " + oldCarlot[i].model + "<p class='description' id='" + i + "'>" + oldCarlot[i].description + "<p class='price'>" + oldCarlot[i].price + " " + "Dollars" + "</p>";
       newCar.innerHTML = createDisplay;
       document.getElementById("outputDiv").appendChild(newCar);
