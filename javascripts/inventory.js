d2bb5ffa8d5c6831703d5baad97dfbd1969c17b7
Oliver White <white_oliver@comcast.net>
2016-11-10 10:04:19 -0600

    cleaned up html

--- javascripts/inventory.js
+++ javascripts/inventory.js
@@ -1,18 +1,18 @@
 "use strict";
 var Carlot = (function(oldCarlot){
 
-  oldCarlot.showCars = function (oldCarlot) {
-    var createDiv = "<div class='row'>";
+  oldCarlot.displayCars = function (oldCarlot) {
+    var createDisplay = "<div class='row'>";
     for (var i = 0; i < oldCarlot.length; i++){
       var newCar = document.createElement("div");
       newCar.classList = "col-sm-4 styled";
-      createDiv = "<p class='make'>" + oldCarlot[i].year +  " " + oldCarlot[i].make + " " + oldCarlot[i].model + "<p class='description' id='" + i + "'>" + oldCarlot[i].description + "<p class='price'>" + oldCarlot[i].price + " " + "Dollars" + "</p>";
-      newCar.innerHTML = createDiv;
+      createDisplay = "<p class='make'>" + oldCarlot[i].year +  " " + oldCarlot[i].make + " " + oldCarlot[i].model + "<p class='description' id='" + i + "'>" + oldCarlot[i].description + "<p class='price'>" + oldCarlot[i].price + " " + "Dollars" + "</p>";
+      newCar.innerHTML = createDisplay;
       document.getElementById("outputDiv").appendChild(newCar);
     }
   } 
 
-  Carlot.loadInventory(Carlot.showCars);
+  Carlot.loadInventory(Carlot.displayCars);
   return oldCarlot
 
 })(Carlot || {});
